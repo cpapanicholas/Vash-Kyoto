fe5294658b90e787089552f9caefbe0b21b943d3
cpapanicholas <cpapanicholas@gmail.com>
2023-10-23 22:15:58 -0500

    added post code

--- Develop/public/assets/js/server.js
+++ Develop/public/assets/js/server.js
@@ -10,10 +10,23 @@ app.get('/api/notes', (req, res) => {
     }
   });
     
-  
   // Define a route to save a new note
   app.post('/api/notes', (req, res) => {
-    // Implement code to save a new note to db.json
+    try {
+      const newNote = req.body;
+      // Read existing notes
+      const notes = JSON.parse(fs.readFileSync('db.json', 'utf8'));
+      // Assign a unique ID to the new note (you can use a library like uuid for this)
+      newNote.id = generateUniqueId(); // Implement generateUniqueId() as needed
+      // Add the new note to the list of notes
+      notes.push(newNote);
+      // Write the updated notes back to the db.json file
+      fs.writeFileSync('db.json', JSON.stringify(notes));
+      res.json(newNote);
+    } catch (error) {
+      // Handle any errors, such as file read/write errors
+      res.status(500).json({ error: 'Failed to save the note' });
+    }
   });
   
   // Define a route to delete a note
