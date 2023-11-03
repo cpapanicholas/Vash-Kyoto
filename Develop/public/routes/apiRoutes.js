c2c36e487e603421822667e322d6ed4005e09af5
cpapanicholas <cpapanicholas@gmail.com>
2023-11-02 20:08:05 -0500

    fixed apiRoutes

--- Develop/public/routes/apiRoutes.js
+++ Develop/public/routes/apiRoutes.js
@@ -12,64 +12,64 @@ router.get('/notes', (req, res) => {
     })
   });
 
-//   // Define a route to save a new note
-// router.post('/notes', (req, res) => {
-//     fs.readFile(path.join(__dirname, 'db.json'), (err, data) => {
-//       if (err) {
-//         res.status(500).json({ error: 'Failed to read notes' });
-//       } else {
-//         const notes = JSON.parse(data);
-//         const newNote = req.body;
+  // Define a route to save a new note
+router.post('/notes', (req, res) => {
+    fs.readFile(path.join(__dirname, 'db.json'), (err, data) => {
+      if (err) {
+        res.status(500).json({ error: 'Failed to read notes' });
+      } else {
+        const notes = JSON.parse(data);
+        const newNote = req.body;
   
-//         // Generate a unique ID for the new note
-//         newNote.id = generateUniqueId(); // Implement the generateUniqueId function
+        // Generate a unique ID for the new note
+        newNote.id = generateUniqueId(); // Implement the generateUniqueId function
   
-//         // Add the new note to the array of notes
-//         notes.push(newNote);
+        // Add the new note to the array of notes
+        notes.push(newNote);
   
-//         // Write the updated notes array back to db.json
-//         fs.writeFile(path.join(__dirname, 'db.json'), JSON.stringify(notes), (err) => {
-//           if (err) {
-//             res.status(500).json({ error: 'Failed to save the note' });
-//           } else {
-//             res.json(newNote);
-//           }
-//         });
-//       }
-//     });
-//   });
+        // Write the updated notes array back to db.json
+        fs.writeFile(path.join(__dirname, 'db.json'), JSON.stringify(notes), (err) => {
+          if (err) {
+            res.status(500).json({ error: 'Failed to save the note' });
+          } else {
+            res.json(newNote);
+          }
+        });
+      }
+    });
+  });
 
-//   // Define a route to delete a note
-// router.delete('/notes/:id', (req, res) => {
-//     const noteId = req.params.id;
+  // Define a route to delete a note
+router.delete('/notes/:id', (req, res) => {
+    const noteId = req.params.id;
   
-//     fs.readFile(path.join(__dirname, 'db.json'), (err, data) => {
-//       if (err) {
-//         res.status(500).json({ error: 'Failed to read notes' });
-//       } else {
-//         const notes = JSON.parse(data);
+    fs.readFile(path.join(__dirname, 'db.json'), (err, data) => {
+      if (err) {
+        res.status(500).json({ error: 'Failed to read notes' });
+      } else {
+        const notes = JSON.parse(data);
   
-//         // Find the index of the note to delete
-//         const index = notes.findIndex((note) => note.id === noteId);
+        // Find the index of the note to delete
+        const index = notes.findIndex((note) => note.id === noteId);
   
-//         if (index !== -1) {
-//           // Remove the note with the given ID
-//           notes.splice(index, 1);
+        if (index !== -1) {
+          // Remove the note with the given ID
+          notes.splice(index, 1);
   
-//           // Write the updated notes array back to db.json
-//           fs.writeFile(path.join(__dirname, 'db.json'), JSON.stringify(notes), (err) => {
-//             if (err) {
-//               res.status(500).json({ error: 'Failed to delete the note' });
-//             } else {
-//               res.json({ message: 'Note deleted successfully' });
-//             }
-//           });
-//         } else {
-//           res.status(404).json({ error: 'Note not found' });
-//         }
-//       }
-//     });
-//   });
+          // Write the updated notes array back to db.json
+          fs.writeFile(path.join(__dirname, 'db.json'), JSON.stringify(notes), (err) => {
+            if (err) {
+              res.status(500).json({ error: 'Failed to delete the note' });
+            } else {
+              res.json({ message: 'Note deleted successfully' });
+            }
+          });
+        } else {
+          res.status(404).json({ error: 'Note not found' });
+        }
+      }
+    });
+  });
 
   
 function generateUniqueId() {
