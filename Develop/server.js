d3b3a4d9845d7af80b38cff06416321b834f1bd3
cpapanicholas <cpapanicholas@gmail.com>
2023-11-03 10:09:14 -0500

    tutor help

--- Develop/server.js
+++ Develop/server.js
@@ -1,13 +1,13 @@
 const express = require('express');
-const htmlRoutes = require("./public/routes/htmlRoutes")
-const apiRoutes = require("./public/routes/apiRoutes")
+const htmlRoutes = require("./routes/htmlRoutes")
+const apiRoutes = require("./routes/apiRoutes")
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static('./public'))
+app.use(express.static('public'))
 app.use("/api", apiRoutes)
 app.use("/", htmlRoutes);
 
